4d738dd7a46e605cd1634b88058e6768e9f66607
chrisAmbagtsheer <chris.ambagtsheer@wur.nl>
2026-09-13 23:24:51 +0200

    CA: added a date checker to Transaction and a method that prints out whether the date is indeed valid.

--- src/com/chrisambagtsheer/bankinsight/Main.java
+++ src/com/chrisambagtsheer/bankinsight/Main.java
@@ -6,97 +6,21 @@ import java.util.List;
 public class Main {
 	
 	public static void main(String[] args) {
-//		// Variable types; String, int, double, boolean. 
-//		// Name the variables with camelCase
-//		// [type] [name] = [value];
-//		String userName = "Chris Ambagtsheer";
-//		int transactionAmount = 500;
-//		double balance = 1000.0;
-//		boolean isTransactionSuccessful = false;
-//		
-//		// Declare constants with final and use SCREAMING_SNAKE_CASE
-//		// final [type] [name] = [value];
-//		final int NUMBER_OF_ACCOUNTS = 1;
-//		printInitializedVariables(userName, transactionAmount, balance, isTransactionSuccessful, NUMBER_OF_ACCOUNTS);
-		
-//		// Write double values always with a decimal for clarity.
-//		double[] transactions = {0.0, 10.0, 100.0, 500.0, 5.0};
-//		// Define an array either with values, or reserve its space in memory
-//		double[] amounts = new double[5];
-//		double totalAmount = transactionSummedTotal(transactions);
-//		System.out.println(totalAmount);
-	
-//		String[] categories = {"Groceries", "University", "Culture", "Housing", "Sports"};
-//		double[] amounts = {300.0, 50.0, 10.0, 1500.0, 40.0};
-//		printAmountPerCategory(categories, amounts);
-		
-//		// Define a new object from the Transaction class
-//		Transaction t = new Transaction(50.0, "Groceries", "2026-09-01", false);
-//		printTransaction(t);
-		
-		// Create a list of Transaction objects that can infitely grow.
-		// Use .get(), .size() and .add()
+
 		List<Transaction> transactions = new ArrayList<>();
 		transactions.add(new Transaction(50.0, "Groceries", "2026-09-01", false));
-		transactions.add(new Transaction(800.0, "Housing", "2026-09-02", false));
+		transactions.add(new Transaction(800.0, "Housing", "2026/09-02", false));
+		transactions.add(new Transaction(432.98, "Sports", "202-09-02", false));
+		transactions.add(new Transaction(1500.0, "Salary", "2026-09-02-10", true));
 		printAllTransactions(transactions);
-//		printAllTransactionsShort(transactions);
 		
-		System.out.print("done!");
+		System.out.println("done!");
 	}
-
-//	private static void printInitializedVariables(String userName, int transactionAmount, double balance,
-//			boolean isTransactionSuccessful, final int numberOfAccounts) {
-//		// Print the variables
-//		System.out.println(userName + " " +
-//				transactionAmount + " " +
-//				balance + " " +
-//				isTransactionSuccessful + " " +
-//				numberOfAccounts);
-//	}
-//
-//	private static double transactionSummedTotal(double[] transactions) {
-//		// Declare summing variable to use within for loop
-//		double totalAmount = 0.0;
-//		
-//		// Loop over the array
-//		// for (starting index); (exit condition); (increase index) {}
-//		for (int i = 0; i < transactions.length; i++) {
-//			
-//			// += is short for variable = variable + value
-//			// ++ is short for variable = variable + 1
-//			totalAmount += transactions[i];
-//		}
-//		return totalAmount;
-//	}
-//
-//	private static void printAmountPerCategory(String[] categories, double[] amounts) {
-//		for (int i = 0; i < categories.length; i++) {
-//			
-//			if (categories[i].equals("University")) {
-//				System.out.println("This month's expense for University is: 250 euro extra");
-//				System.out.print("Total amount for university: ");
-//				System.out.println(amounts[i] + 250);
-//			} else {
-//				System.out.println(categories[i] + " : " + amounts[i]);
-//			}		
-//		}
-//	}
-//	
-//	private static void printTransaction(Transaction t) {
-//		System.out.println(t.category + " : " + t.amount);
-//	}
 	
 	private static void printAllTransactions(List<Transaction> transactions) {
-		for (int i = 0; i < transactions.size(); i++) {
-			Transaction t = transactions.get(i);
+		for (Transaction t : transactions) {
 			System.out.println(t.category + " : " + t.amount);
 		}
 	}
 	
-//	private static void printAllTransactionsShort(List<Transaction> transactions) {
-//		for (Transaction t : transactions) {
-//			System.out.println(t.category + " : " + t.amount);
-//		}
-//	}
 }
--- src/com/chrisambagtsheer/bankinsight/Transaction.java
+++ src/com/chrisambagtsheer/bankinsight/Transaction.java
@@ -1,23 +1,57 @@
 package com.chrisambagtsheer.bankinsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transaction {
-	
-	// fields (instance  variables) that can be used throughout the class
+
+	// fields (instance variables) that can be used throughout the class
 	double amount;
 	String category;
 	String date;
 	boolean isIncome;
-	
+
 	// constructor; special method that takes arguments to set initial values to be
-	// used throughout the class. 
+	// used throughout the class.
 	public Transaction(double amount, String category, String date, boolean isIncome) {
-		
+
 		// Write this.variableName when the same variable name is in the constructor as
-		// used throughout the class as convention.
+		// used throughout the class.
 		this.amount = amount;
 		this.category = category;
 		this.date = date;
 		this.isIncome = isIncome;
+
+		checkDate();
 	}
-	
+
+	private boolean isValidDate(String date) {
+		int index = 0;
+		boolean validation = true;
+
+		if (date.length() != 10) {
+			validation = false;
+		}
+
+		while (index < date.length()) {
+			boolean checkPositions = index == 4 || index == 7;
+			if (!checkPositions && !Character.isDigit(date.charAt(index))) {
+				validation = false;
+			} else if (checkPositions && date.charAt(index) != '-') {
+				validation = false;
+			}
+			index++;
+		}
+		return validation;
+	}
+
+	private void checkDate() {
+		boolean validDate = isValidDate(date);
+		if (validDate) {
+			System.out.println("The date check out.");
+		} else {
+			System.out.println("The date is invalid.");
+		}
+	}
+
 }
